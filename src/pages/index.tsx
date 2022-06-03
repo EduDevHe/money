7ab7eda972a0f0e9fbf4ac1290b831bfecfb5608
devlulcas <lucasalvesrego.contato@gmail.com>
2022-06-02 23:20:09 -0300

    refactor: project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Money</title>
+				<meta name="description" content="Take care of your money" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main>
+				<h1>Money</h1>
+			</main>
+		</>
+	);
+};
+
+export default Home;
