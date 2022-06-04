eae4d5bde1225df64cf1483523342c8223bdd572
devlulcas <lucasalvesrego.contato@gmail.com>
2022-06-04 00:36:20 -0300

    feat: pages

--- src/components/NavLinks/index.tsx
+++ src/components/NavLinks/index.tsx
@@ -1,14 +1,32 @@
+import Link from "next/link";
 import { NavItem } from "../NavItem";
 
 function NavLinks() {
 	return (
 		<>
-			<NavItem>Opa</NavItem>
-			<NavItem>Opa</NavItem>
-			<NavItem>Opa</NavItem>
-			<NavItem>Opa</NavItem>
-			<NavItem>Opa</NavItem>
-			<NavItem>Opa</NavItem>
+			<NavItem>
+				<Link href="/dashboard">Dashboard</Link>
+			</NavItem>
+
+			<NavItem>
+				<Link href="/budget">Orçamento</Link>
+			</NavItem>
+
+			<NavItem>
+				<Link href="/intake">Entradas</Link>
+			</NavItem>
+
+			<NavItem>
+				<Link href="/expenses">Gastos</Link>
+			</NavItem>
+
+			<NavItem>
+				<Link href="/objectives">Objetivos</Link>
+			</NavItem>
+
+			<NavItem>
+				<Link href="/categories">Categorias</Link>
+			</NavItem>
 		</>
 	);
 }
---
+++ src/pages/objectives.tsx
@@ -0,0 +1,3 @@
+export default function Objectives() {
+	return <h1>Objectives</h1>;
+}
