a1bb4efc0dfac78d7e24fcc95dcdebebb79eec68
devlulcas <lucasalvesrego.contato@gmail.com>
2022-06-08 00:04:45 -0300

    refactor: changed location of templates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "../styles/reset.css";
 import "../styles/variables.css";
 import type { AppProps } from "next/app";
-import { MainLayout } from "../layouts/MainLayout";
+import { MainLayout } from "../templates/MainLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
